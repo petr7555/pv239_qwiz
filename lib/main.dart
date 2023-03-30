12aa06eee79ef8457d4f52f1c86a371cf90c05b4
Petr Janik <peta.janik@email.cz>
2023-03-30 08:22:43 +0200

    Add Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pv239_qwiz/common/widget/app_root.dart';
+import 'package:pv239_qwiz/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // IoCContainer.initialize();
+
   runApp(const AppRoot());
 }
