7f0dd1abf352fdda991cbc93fa4410fb66ec12b7
Petr Janik <peta.janik@email.cz>
2023-03-30 08:23:58 +0200

    Initialize IoCContainer

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:pv239_qwiz/common/service/ioc_container.dart';
 import 'package:pv239_qwiz/common/widget/app_root.dart';
 import 'package:pv239_qwiz/firebase_options.dart';
 
@@ -7,7 +8,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  // IoCContainer.initialize();
+  IoCContainer.initialize();
 
   runApp(const AppRoot());
 }
