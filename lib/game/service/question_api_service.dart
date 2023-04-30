6110bf1de878ec97fb27039990f73a3440e677a6
Petr Janik <peta.janik@email.cz>
2023-04-30 15:30:25 +0200

    Add TODO comment

--- lib/game/service/question_api_service.dart
+++ lib/game/service/question_api_service.dart
@@ -4,6 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:pv239_qwiz/common/util/shared_logic_constants.dart';
 import 'package:pv239_qwiz/game/model/question.dart';
 
+// TODO set to false
 const shouldMockQuestion = true;
 final mockQuestion = Question(
   id: '1',
