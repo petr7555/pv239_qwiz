ef04ea0510f17d809051ef718fa06a32f734dd84
Martin Striezovsky <martin.striezovsky@gmail.com>
2023-05-09 11:49:57 +0200

    formatting

--- lib/history/service/history_service.dart
+++ lib/history/service/history_service.dart
@@ -4,8 +4,9 @@ import '../../game/model/game.dart';
 
 class HistoryService {
   static Stream<List<Game>> getGamesByPlayer(String id) {
-    return FirebaseFirestore.instance.collection('games').snapshots().map((snapshot) =>
-        snapshot.docs.map((doc) => Game.fromJson(doc.data())).where((element) => element.players.keys.contains(id))
-            .toList());
+    return FirebaseFirestore.instance.collection('games').snapshots().map((snapshot) => snapshot.docs
+        .map((doc) => Game.fromJson(doc.data()))
+        .where((element) => element.players.keys.contains(id))
+        .toList());
   }
 }
