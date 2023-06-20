d2c59a58af44171607ee78cfb7a5051ae396db3e
Petr Janik <peta.janik@email.cz>
2023-06-20 15:00:42 +0200

    Scroll to current user in leaderboard

--- lib/leaderboard/widget/players_list.dart
+++ lib/leaderboard/widget/players_list.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pv239_qwiz/auth/service/auth_cubit.dart';
 import 'package:pv239_qwiz/game/model/player.dart';
 import 'package:pv239_qwiz/leaderboard/widget/leaderboard_tile.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
-class PlayersList extends StatelessWidget {
+const scrollDurationMilliseconds = 300;
+
+class PlayersList extends StatefulWidget {
   final List<Player> players;
 
   const PlayersList({
@@ -11,11 +16,38 @@ class PlayersList extends StatelessWidget {
   });
 
   @override
+  State<PlayersList> createState() => _PlayersListState();
+}
+
+class _PlayersListState extends State<PlayersList> {
+  final ItemScrollController itemScrollController = ItemScrollController();
+  final ScrollOffsetController scrollOffsetController = ScrollOffsetController();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final userId = context.read<AuthCubit>().userId;
+      final index = widget.players.indexWhere((player) => player.id == userId);
+      if (index == -1) {
+        //TODO test
+        return;
+      }
+      itemScrollController.scrollTo(
+        index: index,
+        duration: const Duration(milliseconds: scrollDurationMilliseconds),
+        curve: Curves.easeInOutCubic,
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      itemCount: players.length,
+    return ScrollablePositionedList.separated(
+      itemCount: widget.players.length,
+      itemScrollController: itemScrollController,
       itemBuilder: (context, index) {
-        final player = players[index];
+        final player = widget.players[index];
         final rank = index + 1;
 
         return LeaderboardTile(rank: rank, player: player);
